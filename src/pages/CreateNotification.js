90c827e4b9542e6bdc9843c3ab6654cce1e07ba8
Keren Horowitz <horowitzkeren@gmail.com>
2022-05-17 20:04:57 +0300

    initial notification page

---
+++ src/pages/CreateNotification.js
@@ -0,0 +1,43 @@
+//import logo from "./logo.svg";
+import "../css/CreateNotification.css";
+import React from "react";
+
+class CreateNotification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A description was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Description:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<CreateNotification />);
+
+export default CreateNotification;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+//import ReactDOM from "react-dom/client";
+import "../css/App.css";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Home Page</p>
+      </header>
+    </div>
+  );
+}
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Home />);
