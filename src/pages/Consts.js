bf42cf9158c1db17dfdc7c479a7180d6f4267a29
rotemik <rotem3971@gmail.com>
2022-05-21 10:31:37 +0300

    add consts file

---
+++ src/pages/Consts.js
@@ -0,0 +1,28 @@
+const HazardTypes = {
+    Roadblock: 1,
+    Roadside: 2,
+    Lost: 3,
+    Found: 4,
+    Other: 5
+};
+
+const RoadblockTypes = {
+    InfrastructureWorks: 1,
+    BlockForPedestrians: 2,
+    OrganizedEvent: 3,
+    SecurityOrCriminalIncident: 4,
+};
+
+const RoadsideTypes = {
+    Police: 1,
+    WildAnimal: 2
+};
+
+const LostAndFoundsTypes = {
+    Jewel: 1,
+    BagOrPurse: 2,
+    Card: 3,
+    Pet: 4,
+    Junk: 5,
+    Other: 6
+};
