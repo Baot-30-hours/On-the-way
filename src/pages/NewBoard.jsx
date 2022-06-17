11dcad5a01fdfe876c7fcf34c601c18d1b128c47
Lior Gazit <lior@classiq.io>
2022-06-17 18:27:10 +0300

    Working news board with iterator

--- src/pages/NewBoard.jsx
+++ src/pages/NewBoard.jsx
@@ -1,63 +1,56 @@
 import React from "react";
+// https://www.pluralsight.com/guides/iterate-through-a-json-response-in-jsx-render-for-reactjs
 
+const Info = ({ streetAddress, fromStreet, toStreet, jobDesctiprion }) => (
+    <div>
+      <div>
+        <p>{streetAddress}</p>
+        <p>{fromStreet}</p>
+        <p>{toStreet}</p>
+        <p>{jobDesctiprion}</p>
+      </div>
+    </div>
+  );
 
 export default class FetchData extends React.Component {
-    // https://www.youtube.com/watch?v=T3Px88x_PsA
-    state = {
-        streets: []
-    };
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            streets_info: [],
+        };
+    
+        this.GetStreetData = this.GetStreetData.bind(this);
+      }
 
     async componentDidMount() {
-        this.getData()
-            .then((res) => this.setState({ response: res.data.features }))
-            .catch((err) => console.log(err));
-    };
+        const streets_info = await this.GetStreetData();
+        this.setState({ streets_info });
+      }
 
-    getData = async() => {
+    async GetStreetData() {
         const url = "https://api.tel-aviv.gov.il/gis/Layer?layerCode=680";
         const response = await fetch(url); //this function is async
         const data = await response.json();
-        if (response.status !== 200) throw Error(data.message);
-        // const d = data.features[0]
-        // console.log(d.attributes.shem_rechov);
-        this.setState({streets: data.features})
-        return data.features
+        const data_features = data.features
+        console.log(data_features)
+        return data_features
     };
-    const Work = ({ street, from_street, to_street, type_of_work }) => (
-        <div>
-            <h3>{street}</h3>
-            <h3>{from_street}</h3>
-            <h3> {to_street} </h3>
-            <h3>{type_of_work} </h3>
 
-        </div>
-    );
-    // async getStreetsData() {
-    //     const res = await fetch("https://api.tel-aviv.gov.il/gis/Layer?layerCode=680");
-    //     const data = await res.json();
-    //     return data.results;
-    // }
-    //
-    // async componentDidMount() {
-    //     const streets = await this.getStreetsData();
-    //     this.setState({ streets });
-    // }
-    render() {
-        return (
-            <div>
-                <h1>Road close - Live updates</h1>
-                {this.state.streets.map((features) => (
-                    <Work
-                        street={JSON.stringify(features.attributes.shem_rechov)}
-                        from_street={JSON.stringify(features.attributes.me_rechov)}
-                        to_street={JSON.stringify(features.attributes.add_rechov)}
-                        type_of_work={JSON.stringify(features.attributes.teur_meshulav)}
-                    />
+   
+
+        render() {
+            return (
+              <div>
+                {this.state.streets_info.map((info) => (
+                  <Info
+                  streetAddress={JSON.stringify(info.attributes.ktovet)}
+                  fromStreet={JSON.stringify(info.attributes.me_rechov)}
+                  toStreet={JSON.stringify(info.attributes.add_rechov)}
+                  jobDesctiprion={JSON.stringify(info.attributes.pirtai_avoda)}
+                  />
                 ))}
-        </div>)
-    }
+              </div>
+            );
+          }
 }
-                // <h3>street = {JSON.stringify(this.state.streets[i].shem_rechov)}</h3>
-                // <h3>from street = {JSON.stringify(this.state.streets[i].me_rechov)}</h3>
-                // <h3> to street = {JSON.stringify(this.state.streets[i].add_rechov)}</h3>
-                // <h3>type of work = {JSON.stringify(this.state.streets[i].teur_meshulav)}</h3>
