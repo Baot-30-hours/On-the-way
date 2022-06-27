93300f7274e61c3727dd1fde24ec82371dff8200
sahar <sahar.fefer@gmail.com>
2022-06-27 20:18:08 +0300

    update Home.css, GoogleMap.jsx and Home.jsx

--- src/pages/GoogleMap.jsx
+++ src/pages/GoogleMap.jsx
@@ -1,59 +1,146 @@
-import React, { useEffect, useState } from 'react'
-import { GoogleMap, useJsApiLoader, StandaloneSearchBox, Marker } from '@react-google-maps/api';
+// import React, { useState, useRef } from 'react'
+// import { GoogleMap, useJsApiLoader, StandaloneSearchBox, Marker } from '@react-google-maps/api';
 
-const containerStyle = {
-  width: '800px',
-  height: '400px'
-};
+// const containerStyle = {
+//   width: '800px',
+//   height: '400px'
+// };
 
-const center = {
-  lat:  -3.745,
-  lng: -38.523
-};
+// // const center = {
+// //   lat: -3.745,
+// //   lng: -38.523
+// // };
 
-function MyComponent() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyBtUSAW7ssnBNngTj4Q7X076cyRoCHtd94"
-  })
+// let markerArray = [];
 
-  const [map, setMap] = useState(null)
-  const [currentLocation, setCurrentLocation] = useState({ lat: -3.745, lng: -38.523 })
+// function MyComponent() {
+//   const { isLoaded } = useJsApiLoader({
+//     id: 'google-map-script',
+//     googleMapsApiKey: "AIzaSyBtUSAW7ssnBNngTj4Q7X076cyRoCHtd94"
+//   })
 
-//   useEffect(() => {
-//     navigator.geolocation.getCurrentPosition(function (position) {  
-//        setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
+//   const [map, setMap] = useState(null)
+//   const [currentLocation, setCurrentLocation] = useState({ lat: -3.745, lng: -38.523 })
+//   const [markers, setMarks] = useState([])
+//   const [bounds, setBounds] = useState(null)
+//   const searchBox = useRef(null);
+
+//   const onLoad = React.useCallback(function callback(map) {
+//     setBounds(new window.google.maps.LatLngBounds(currentLocation));
+//     map.fitBounds(bounds);
+//     setMap(map)
+//     navigator.geolocation.getCurrentPosition(function (position) {
+//       setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
 //     });
-// }, [])
+//     //     navigator.geolocation.getCurrentPosition(function(position){
+//     // });
+//   }, [])
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(currentLocation);
-    map.fitBounds(bounds);
-    setMap(map)
-    navigator.geolocation.getCurrentPosition(function (position) {  
-        setCurrentLocation({ lat: position.coords.latitude, lng: position.coords.longitude })
-     });
-//     navigator.geolocation.getCurrentPosition(function(position){
-// });
-  }, [])
+//   const onUnmount = React.useCallback(function callback(map) {
+//     setMap(null)
+//   }, [])
 
-  
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+//   const onSBLoad = ref => {
+//     searchBox = ref;
+//   };
 
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={currentLocation}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+//   const onPlacesChanged = () => {
+//     markerArray = [];
+//     let results = searchBox.getPlaces();
+//     for (let i = 0; i < results.length; i++) {
+//       let place = results[i].geometry.location;
+//       markerArray.push(place);
+//     }
+//     setMarks({ markers: markerArray });
+//     console.log(markerArray);
+//   };
+
+//   return isLoaded ? (
+//       <GoogleMap
+//         mapContainerStyle={containerStyle}
+//         center={currentLocation}
+//         zoom={10}
+//         onLoad={onLoad}
+//         onUnmount={onUnmount}
+//       >
+//         { /* Child components, such as markers, info windows, etc. */}
+//       </GoogleMap>
+//   ) : <></>
+// }
+
+// export default React.memo(MyComponent)
+
+/*global google*/
+import React, { useState } from "react";
+import '../css/GoogleMap.css';
+
+import { GoogleMap, StandaloneSearchBox, Marker } from "@react-google-maps/api";
+
+let markerArray = [];
+const SimpleMap = () => {
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 })
+  const [markers, setMarkers] = useState([])
+  const [bounds, setBounds] = useState(null)
+  const [searchBox, setSearchBox] = useState(null)
+
+  const onMapLoad = map => {
+    navigator?.geolocation.getCurrentPosition(
+      ({ coords: { latitude: lat, longitude: lng } }) => {
+        const pos = { lat, lng };
+        setCurrentLocation({ currentLocation: pos });
+      }
+    );
+    google.maps.event.addListener(map, "bounds_changed", () => {
+      console.log(map.getBounds());
+      setBounds(map.getBounds());
+    });
+  };
+
+  const onSBLoad = ref => {
+    setSearchBox(ref);
+    console.log('ref', ref);
+  };
+
+  const onPlacesChanged = () => {
+    markerArray = [];
+    let results = searchBox.getPlaces();
+    for (let i = 0; i < results.length; i++) {
+      let place = results[i].geometry.location;
+      markerArray.push(place);
+    }
+    setMarkers(markerArray);
+    console.log(markerArray);
+  };
+
+  return (
+    <div className="map-wrapper">
+      <div id="searchbox">
+        <StandaloneSearchBox
+          onLoad={onSBLoad}
+          onPlacesChanged={onPlacesChanged}
+          bounds={bounds}
+        >
+          <input
+            type="text"
+            placeholder="Customized your placeholder"
+          />
+        </StandaloneSearchBox>
+      </div>
+      <br />
+      <div>
+        <GoogleMap
+          center={currentLocation}
+          zoom={5}
+          onLoad={map => onMapLoad(map)}
+          mapContainerStyle={{ height: "400px", width: "800px" }}
+        >
+          {markers.map((mark, index) => (
+            <Marker key={index} position={mark} />
+          ))}
+        </GoogleMap>
+      </div>
+    </div>
+  );
 }
 
-export default React.memo(MyComponent)
+export default SimpleMap;
